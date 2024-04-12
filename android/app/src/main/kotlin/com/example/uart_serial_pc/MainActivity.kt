8c65a6df8452e05a6ec1cd054b8acd1154cca417
Bob Taylor <btaylor@allytechllc.com>
2024-04-12 08:02:13 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/uart_serial_pc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.uart_serial_pc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/source/uart_serial.dart
@@ -0,0 +1,19 @@
+import 'package:dart_periphery/dart_periphery.dart';
+import 'package:flutter/foundation.dart';
+
+class UartSerial {
+  // ignore: prefer_typing_uninitialized_variables
+  var s;
+
+  void initUartSerial() {
+    debugPrint('');
+  }
+
+  void writeUartSerial() {
+    debugPrint('');
+  }
+
+  void readUartSerial() {
+    debugPrint('');
+  }
+}
